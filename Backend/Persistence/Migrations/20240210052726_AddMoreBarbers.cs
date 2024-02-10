d31660331be57c57a1b8d8190c869287b8e18894
Igor Jelic <igor.jelic.98@gmail.com>
2024-02-10 07:44:10 +0100

    Styling home page

---
+++ Backend/Persistence/Migrations/20240210052726_AddMoreBarbers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreBarbers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Barbers",
+                columns: new[] { "Id", "Username", "Password", "IsDeleted", "Gender" },
+                values: new object[,]{
+                    {Guid.NewGuid(), "Milica", "pasword3", false, 1},
+                    {Guid.NewGuid(), "Natalija", "password4", false, 1},
+                    {Guid.NewGuid(), "Jovan", "pasword5", false, 0},
+                    {Guid.NewGuid(), "Dragan", "password6", false, 0},
+                    {Guid.NewGuid(), "Katarina", "pasword7", false, 1},
+                    {Guid.NewGuid(), "Uros", "password8", false, 0},
+                    {Guid.NewGuid(), "Bosiljka", "password9", false, 1},
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- frontend/src/components/home/BarberCard.jsx
+++ frontend/src/components/home/BarberCard.jsx
@@ -1,5 +1,7 @@
 import styles from './Home.module.css';
 import { formatRating } from '../../utils/formatUtils/ratingUtils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faScissors } from '@fortawesome/free-solid-svg-icons';
 
 export default function BarberCard({barber}){
 
@@ -8,14 +10,16 @@ export default function BarberCard({barber}){
     return(
         <>
             <div className={styles.card}>
+                <FontAwesomeIcon icon={faScissors} className={styles.cardLogo}/>
+                <FontAwesomeIcon icon={faScissors} className={styles.cardBtn}/>
                 <h2 className={styles.title}>{barber.username}</h2>
+                <hr className={styles.line}/>
                 <div className={styles.info}>Gender: {gender}</div>
                 <div className={styles.info}>Rating: <span className={styles.rating}>{formatRating(barber.rating)}</span></div>
                 <div className={styles.info}>
                     Appointments so far: <span className={styles.count}>{barber.appointmentsCount}</span>
                 </div>
-                <hr className={styles.line}/>
-                <button className={styles.btn}>Details</button>
+                {/* <button className={styles.btn}>Details</button> */}
             </div>
         </>
     )
